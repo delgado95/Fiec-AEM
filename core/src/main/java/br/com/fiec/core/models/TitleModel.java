c5bb69c40ee14cab8ad9d29e934d9d3da897144d
delgado95 <delgado.jd.2003@gmail.com>
2026-02-06 15:58:21 -0300

    Initial commit - AEM project

---
+++ core/src/main/java/br/com/fiec/core/models/TitleModel.java
@@ -0,0 +1,37 @@
+package br.com.fiec.core.models;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+
+@Model(adaptables = SlingHttpServletRequest.class, adapters = {
+        ComponentExporter.class }, resourceType = TitleModel.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public class TitleModel implements ComponentExporter {
+
+    static final String RESOURCE_TYPE = "eventsmap/components/event";
+
+    @ValueMapValue
+    private String title;
+
+    @ValueMapValue
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String getExportedType() {
+        return RESOURCE_TYPE;
+    }
+}
